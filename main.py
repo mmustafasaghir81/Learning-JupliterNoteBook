5ab6745ecce0f683c22ff53880d8f60ce6ef9b67
mmustafasaghir <akifsaghirai@gmail.com>
2024-09-05 14:32:41 +0500

    Learning-JupiterNoteBook
    Learning-JupiterNoteBook

---
+++ main.py
@@ -0,0 +1,24 @@
+name:str = "Muhammad Mustafa Saghir"
+print(name)
+
+father_name:str = "Muhammad Saghir"
+print(father_name)
+
+name:str = "Muhammad Mustafa Saghir"
+print(name)
+name = 100
+print("Welcome")
+print(name)
+
+name:str = "Muhammad Mustafa Saghir"
+print( type (name) )
+print( id (name) )
+print( dir (name) )
+
+name:str = "Muhammad Mustafa Saghir"
+#object.method()
+#object.attribute
+ 
+print( name.capitalize() )
+print( name.casefold() )
+print( name.center(35) )
